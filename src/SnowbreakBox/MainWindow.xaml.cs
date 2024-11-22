95c981af318eb0ceb86cd7f0a3a1395cba8a2e46
TeapotMoe <mncok10@gmail.com>
2024-11-22 20:44:43 +0800

    feat: 支持修正存档位置

--- src/SnowbreakBox/MainWindow.xaml.cs
+++ src/SnowbreakBox/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace SnowbreakBox {
 		private Version _gameVersion;
 		private readonly HttpClient _httpClient = new HttpClient();
 		private Task<Version> _fetchRemoteGameVersionTask;
+		private bool _isSavedPathStandard = false;
 
 		private enum LauncherType {
 			Classic,
@@ -58,37 +59,47 @@ namespace SnowbreakBox {
 		public int GraphicState {
 			get {
 				// 游戏启动时会修改 Engine.ini，删除注释并重新排列条目，很难根据 Engine.ini
-				// 判断画质等级。因此我们将画质等级保存为设置。
-				int setting = Settings.Default.GraphicState;
-				if (setting == 0) {
+				// 判断画质等级。因此我们将画质等级保存在单独的文件里。
+				string engineIniPath = Path.Combine(_savedFolder, ENGINE_INI_PATH);
+				string storagePath = engineIniPath + ".box";
+				if (!File.Exists(storagePath)) {
 					return 0;
 				}
 
 				try {
-					// 判断有没有画质补丁相对容易
-					string engineIniPath = Path.Combine(_savedFolder, ENGINE_INI_PATH);
-					using (StreamReader sr = new StreamReader(engineIniPath)) {
-						string content = sr.ReadToEnd();
-						if (content.IndexOf("[SystemSettings]") == -1) {
+					byte[] bytes = File.ReadAllBytes(storagePath);
+					if (bytes.Length == 4) {
+						int value = BitConverter.ToInt32(bytes, 0);
+
+						// 简单验证 Engine.ini 有没有应用补丁
+						if (File.ReadAllText(engineIniPath).IndexOf("[SystemSettings]") == -1) {
+							// Engine.ini 和存储的值不一致
+							File.Delete(storagePath);
 							return 0;
 						} else {
-							return setting;
+							return value;
 						}
 					}
-				} catch {
-					return 0;
-				}
+				} catch { }
+				
+				return 0;
 			}
 			set {
+				string engineIniPath = Path.Combine(_savedFolder, ENGINE_INI_PATH);
+				string storagePath = engineIniPath + ".box";
+
 				try {
 					// 无需保留原始文件的内容，游戏启动时会自动添加默认条目
-					string engineIniPath = Path.Combine(_savedFolder, ENGINE_INI_PATH);
 					string iniText = value > 0 ?
 						Properties.Resources.ResourceManager.GetString("Profile" + value) : string.Empty;
 					File.WriteAllText(engineIniPath, iniText);
 
-					Settings.Default.GraphicState = value;
-					Settings.Default.Save();
+					if (value == 0 && File.Exists(storagePath)) {
+						File.Delete(storagePath);
+						return;
+					}
+
+					File.WriteAllBytes(storagePath, BitConverter.GetBytes(value));
 				} catch (Exception ex) {
 					ShowError(ex.Message);
 				}
@@ -135,6 +146,14 @@ namespace SnowbreakBox {
 			}
 		}
 
+		public Visibility LaunchButtonVisibility => _gameHasUpdate ? Visibility.Collapsed : Visibility.Visible;
+		public Visibility UpdateButtonVisibility => _gameHasUpdate ? Visibility.Visible : Visibility.Collapsed;
+
+		public bool IsFixSavedPathButtonEnabled => !_isSavedPathStandard;
+
+		public Visibility FixSavedPathWarningInfoBarVisibility => _isSavedPathStandard ? Visibility.Collapsed : Visibility.Visible;
+		public Visibility FixSavedPathSuccessInfoBarVisibility => _isSavedPathStandard ? Visibility.Visible : Visibility.Collapsed;
+
 		private void WaitForCheckingGameUpdate() {
 			if (_fetchRemoteGameVersionTask == null || _gameVersion == null) {
 				return;
@@ -149,23 +168,18 @@ namespace SnowbreakBox {
 			GameHasUpdate = remoteVersion > _gameVersion;
 		}
 
-		public Visibility LaunchButtonVisibility {
-			get => _gameHasUpdate ? Visibility.Collapsed : Visibility.Visible;
-		}
-
-		public Visibility UpdateButtonVisibility {
-			get => _gameHasUpdate ? Visibility.Visible : Visibility.Collapsed;
-		}
-
 		private bool IsGameRunning() {
 			return NativeMethods.FindWindow("UnrealWindow", "尘白禁区") != IntPtr.Zero;
 		}
 
 		private static void ShowError(string msg) {
-			System.Windows.MessageBox.Show(
-				msg, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+			MessageBox.Show(msg, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
 		}
 
+		// saveFolderType取值
+		// 0: 其他
+		// 1: 标准位置 (GameFolder\game\Saved)
+		// 2: 旧版西山居启动器位置(没有 game 中间目录)
 		private bool FindGame(
 			string launcherRegKey,
 			string gameRegKey,
@@ -173,11 +187,11 @@ namespace SnowbreakBox {
 			out string launcherPath,
 			out string gameFolder,
 			out string savedFolder,
-			out bool isSeasunOld
+			out int saveFolderType
 		) {
 			gameFolder = null;
 			savedFolder = null;
-			isSeasunOld = false;
+			saveFolderType = 0;
 
 			try {
 				launcherPath = Registry.GetValue(launcherRegKey, "DisplayIcon", null) as string;
@@ -185,7 +199,9 @@ namespace SnowbreakBox {
 					launcherPath = null;
 					return false;
 				}
-			} catch (Exception) {
+
+				launcherPath = launcherPath.Replace('/', '\\');
+			} catch {
 				launcherPath = null;
 				return false;
 			}
@@ -199,15 +215,18 @@ namespace SnowbreakBox {
 
 				// 西山居启动器保存的路径包含正斜杠
 				gameFolder = gameFolder.Replace('/', '\\');
-			} catch (Exception) {
+			} catch {
 				gameFolder = null;
 				return false;
 			}
 
 			try {
 				// 检查存档，可能位于游戏文件夹内或 %LocalAppData%，应优先检查游戏文件夹
+
+				// 优先使用标准位置
 				string engineIniPath = Path.Combine(gameFolder, "game\\Saved", ENGINE_INI_PATH);
 				if (File.Exists(engineIniPath)) {
+					saveFolderType = 1;
 					savedFolder = Path.Combine(gameFolder, "game\\Saved");
 					return true;
 				}
@@ -216,8 +235,8 @@ namespace SnowbreakBox {
 					// 旧版西山居启动器没有 game 中间目录
 					engineIniPath = Path.Combine(gameFolder, "Saved", ENGINE_INI_PATH);
 					if (File.Exists(engineIniPath)) {
+						saveFolderType = 2;
 						savedFolder = Path.Combine(gameFolder, "Saved");
-						isSeasunOld = true;
 						return true;
 					}
 
@@ -239,8 +258,7 @@ namespace SnowbreakBox {
 					savedFolder = Path.Combine(localAppDataFolder, "Game\\Saved");
 					return true;
 				}
-			} catch (Exception) {
-			}
+			} catch { }
 
 			savedFolder = null;
 			return false;
@@ -283,7 +301,7 @@ namespace SnowbreakBox {
 				out string classicLauncherPath,
 				out string classicGameFolder,
 				out string classicSavedFolder,
-				out _
+				out int clssicSavedFolderType
 			);
 			// 检测西山居启动器
 			bool seasunDetected = FindGame(
@@ -293,7 +311,7 @@ namespace SnowbreakBox {
 				out string seasunLauncherPath,
 				out string seasunGameFolder,
 				out string seasunSavedFolder,
-				out bool isSeasunOld
+				out int seasunSavedFolderType
 			);
 
 			if (classicDetected && seasunDetected) {
@@ -317,6 +335,8 @@ namespace SnowbreakBox {
 				return false;
 			}
 
+			_isSavedPathStandard = clssicSavedFolderType == 1;
+
 			if (classicDetected) {
 				_launcherType = LauncherType.Classic;
 				_launcherPath = classicLauncherPath;
@@ -328,14 +348,17 @@ namespace SnowbreakBox {
 					// 读取 manifest.json 而不是 version.cfg，启动器使用前者对比本地与云端的文件变更
 					string json = File.ReadAllText(Path.Combine(GameFolder, "manifest.json"));
 					string versionStr = ReadStringFromJson(json, "version");
-					if (versionStr == null || !Version.TryParse(versionStr, out _gameVersion)) {
+					if (versionStr == "--") {
+						// 存在更新时经典启动器将 version 字段改为“--”
+						_gameVersion = new Version();
+					} else if (versionStr == null || !Version.TryParse(versionStr, out _gameVersion)) {
 						_gameVersion = null;
 					}
 				} catch (Exception e) {
 					ShowError("读取游戏版本失败：" + e.Message);
 				}
 			} else {
-				_launcherType= isSeasunOld ? LauncherType.SeasunOld : LauncherType.Seasun;
+				_launcherType = seasunSavedFolderType == 2 ? LauncherType.SeasunOld : LauncherType.Seasun;
 				_launcherPath = seasunLauncherPath;
 				GameFolder = seasunGameFolder;
 				_savedFolder = seasunSavedFolder;
@@ -354,8 +377,14 @@ namespace SnowbreakBox {
 
 		private bool LaunchGame() {
 			string arguments = "-FeatureLevelES31 -ChannelID=jinshan ";
+
 			// 两个启动器传递路径的做法都是错的，这导致了存档路径的混乱。为了使用现有存档，我们只能将错就错。
-			if (_launcherType == LauncherType.Classic) {
+			if (_isSavedPathStandard) {
+				// 使用标准存档路径
+				arguments += "\"-userdir=";
+				arguments += Path.Combine(GameFolder, "game");
+				arguments += '\"';
+			} else if (_launcherType == LauncherType.Classic) {
 				// 经典启动器在路径两边加反斜杠和双引号
 				arguments += "\"-userdir=\\\"";
 				arguments += Path.Combine(GameFolder, "game");
@@ -406,12 +435,14 @@ namespace SnowbreakBox {
 		private async Task<Version> FetchRemoteGameVersion() {
 			try {
 				string response = await _httpClient.GetStringAsync(
-					"https://cbjq-client.xoyocdn.com/games/cbjq/SyncEntrys/cbjq_SyncEntry.json").ConfigureAwait(false);
+					"https://cbjq-client.xoyocdn.com/games/cbjq/SyncEntrys/cbjq_SyncEntry.json")
+					.ConfigureAwait(false);
 				string versionStr = ReadStringFromJson(response, "GameVersion");
 				if (versionStr != null && Version.TryParse(versionStr, out var version)) {
 					return version;
 				}
-			} catch (HttpRequestException) {
+			} catch (Exception ex) {
+				_ = Dispatcher.BeginInvoke((Action)(() => ShowError("检查游戏更新失败：" + ex.Message)));
 			}
 
 			return null;
@@ -484,6 +515,65 @@ namespace SnowbreakBox {
 					Close();
 				}
 			}
+		}
+
+		static void CopyDirectory(string sourceDir, string destinationDir) {
+			Directory.CreateDirectory(destinationDir);
+
+			// 复制文件
+			foreach (string file in Directory.GetFiles(sourceDir)) {
+				string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+				File.Copy(file, destFile);
+			}
+
+			// 递归复制子文件夹
+			foreach (string dir in Directory.GetDirectories(sourceDir)) {
+				string destDir = Path.Combine(destinationDir, Path.GetFileName(dir));
+				CopyDirectory(dir, destDir);
+			}
+		}
+
+		private void FixSavedPathButton_Click(object sender, RoutedEventArgs e) {
+			if (MessageBox.Show("确定修正存档路径？这个操作是不可逆的。",
+				"警告", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.Cancel) {
+				return;
+			}
+
+			string standardSavedPath = Path.Combine(GameFolder, "game\\Saved");
+
+			try {
+				if (Directory.Exists(standardSavedPath)) {
+					Directory.Delete(standardSavedPath, true);
+				}
+
+				if (string.Compare(Path.GetPathRoot(standardSavedPath),
+					Path.GetPathRoot(_savedFolder), StringComparison.OrdinalIgnoreCase) == 0) {
+					// 位于同一个驱动器上可以移动
+					Directory.Move(_savedFolder, standardSavedPath);
+				} else {
+					// 复制而不是移动，一来跨驱动不存在移动的概念，二来存档文件被占用时不会造成破坏
+					CopyDirectory(_savedFolder, standardSavedPath);
+					Directory.Delete(_savedFolder, true);
+				}
+			} catch (Exception ex) {
+				ShowError(ex.Message);
+				return;
+			}
+
+			try {
+				// 删除旧存档路径上的所有空文件夹
+				while (true) {
+					_savedFolder = Path.GetDirectoryName(_savedFolder);
+					// 如果文件夹不为空会抛出异常
+					Directory.Delete(_savedFolder);
+				}
+			} catch { }
+
+			_savedFolder = standardSavedPath;
+			_isSavedPathStandard = true;
+			OnPropertyChanged(nameof(IsFixSavedPathButtonEnabled));
+			OnPropertyChanged(nameof(FixSavedPathWarningInfoBarVisibility));
+			OnPropertyChanged(nameof(FixSavedPathSuccessInfoBarVisibility));
 		}
 	}
 }
