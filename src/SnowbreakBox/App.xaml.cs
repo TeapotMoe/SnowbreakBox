650b5e67f735ec6209c3b3ac4821290313651e39
TeapotMoe <mncok10@gmail.com>
2024-11-23 12:19:34 +0800

    feat: 遇到未处理异常显示错误消息

--- src/SnowbreakBox/App.xaml.cs
+++ src/SnowbreakBox/App.xaml.cs
@@ -1,6 +1,15 @@
+using System;
 using System.Windows;
 
 namespace SnowbreakBox {
 	public partial class App : Application {
+		App() {
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+		}
+
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			MessageBox.Show(e.ExceptionObject.ToString(),
+				"未处理异常", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
 	}
 }
